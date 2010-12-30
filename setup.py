615e3ae01ee1c65160703533970aeae5c43c08cc
Jason R. Coombs <jaraco@jaraco.com>
2010-12-30 15:48:38 -0500

    Set default increment to 0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 setup_params = dict(
 	name='jaraco.home',
-	use_hg_version=True,
+	use_hg_version={'increment':'0.1'},
 	packages=find_packages(),
 	namespace_packages=['jaraco'],
 	zip_safe=False,
