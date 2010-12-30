377800dffab05a6d5f278b8bd40c97a6da5c4f28
Jason R. Coombs <jaraco@jaraco.com>
2010-12-30 15:48:04 -0500

    Updated to use hgtools

--- setup.py
+++ setup.py
@@ -2,10 +2,13 @@
 from setuptools import setup, find_packages
 setup_params = dict(
 	name='jaraco.home',
-	version='1.0',
+	use_hg_version=True,
 	packages=find_packages(),
 	namespace_packages=['jaraco'],
 	zip_safe=False,
+	setup_requires=[
+		'hgtools',
+	],
 )
 if __name__ == '__main__':
 	setup(**setup_params)
