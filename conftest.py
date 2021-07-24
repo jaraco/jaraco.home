595ab583f8ade25cc4a65e908f2cc462c1ae53a2
Jason R. Coombs <jaraco@jaraco.com>
2021-07-24 02:04:13 -0400

    Skip tests on Windows for now

--- conftest.py
+++ conftest.py
@@ -1,10 +1,16 @@
+import platform
+
 import pytest
 from mockproc import mockprocess
 
 
+win_ignore = [
+    'jaraco/home/hdhomerun.py',
+] * (platform.system() == 'Windows')
+
 collect_ignore = [
     'jaraco/home/relay.py',
-]
+] + win_ignore
 
 
 @pytest.fixture(scope='session', autouse=True)
