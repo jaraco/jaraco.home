959ee7fc8722838a5075c4da8347467c3cc8b877
Jason R. Coombs <jaraco@jaraco.com>
2021-07-24 02:00:13 -0400

    ⚫ Fade to black.

--- conftest.py
+++ conftest.py
@@ -11,6 +11,7 @@ collect_ignore = [
 def hdhomerun_config_mocked():
     # todo: should be jaraco.home.homerun, but for pytest bug
     import home.hdhomerun as hd
+
     hd.hdhomerun_config = 'hdhomerun_config'
     scripts = mockprocess.MockProc()
     scripts.append('hdhomerun_config', returncode=0, stdout="ch=none ss=80\n")
